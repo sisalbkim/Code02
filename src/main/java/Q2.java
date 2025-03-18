913ea31cbe9409628531c6cddfb7c28ddf09cfe3
kimhyengyu <sisalbkim@gmail.com>
2025-03-18 16:18:56 +0900

    2-12 91페이지

---
+++ src/main/java/Q2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Q2 {
+    public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+
+        int a;
+        int b;
+
+
+        System.out.println("첫번째 값을 입력하세요 ==> ");
+        a = s.nextInt();
+        System.out.println(a);
+
+
+        System.out.println("두번째 값을 입력하세요 ==> ");
+        b = s.nextInt();
+        System.out.println(b);
+
+        System.out.println(a + " + " + b + " = " + (a + b));
+
+        s.close();
+
+
+    }
+}
