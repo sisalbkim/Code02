4249f4fefefd5099101f013a5b23ab1b3e3c1206
kimhyengyu <sisalbkim@gmail.com>
2025-03-21 09:01:34 +0900

    책 3-3번 110페이지

---
+++ src/main/java/Code03_03.java
@@ -0,0 +1,14 @@
+public class Code03_03 {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 3;
+        b = 4;
+        c = 5;
+
+        System.out.println(a + b - c);
+        System.out.println(a - c + b);
+        System.out.println(-c + a + b);
+
+
+    }
+}
