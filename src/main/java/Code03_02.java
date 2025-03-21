16dbd775369f09d689c56232dbcf001b8eb35d5c
kimhyengyu <sisalbkim@gmail.com>
2025-03-21 15:28:57 +0900

    03_02번 재작성

--- src/main/java/Code03_02.java
+++ src/main/java/Code03_02.java
@@ -1,18 +1,26 @@
+import java.util.Scanner;
+
 public class Code03_02 {
     public static void main(String[] args) {
-        int n1, n2, res;
-        n1 = 5;
-        n2 = 3;
+        Scanner s = new Scanner(System.in);
+
+        int n1, n2;
+        double res;
+        System.out.println("첫번째 정수를 입력해주세요 : ");
+        n1 = s.nextInt();
+        System.out.println("두번째 정수를 입력해주세요 : ");
+        n2 = s.nextInt();
         res = n1 + n2;
         System.out.println(res);
         res = n1 - n2;
         System.out.println(res);
         res = n1 * n2;
         System.out.println(res);
-        res = n1 / n2;
+        res = (double) n1 / n2;
         System.out.println(res);
         res = n1 % n2;
         System.out.println(res);
 
+        s.close();
     }
 }
