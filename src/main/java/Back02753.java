036d1dddd9bbefb14700fcbae699046c38bc9a60
kimhyengyu <sisalbkim@gmail.com>
2025-03-25 17:38:46 +0900

    백준문제 if문 02753문제.

---
+++ src/main/java/Back02753.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Back02753 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("연도를 입력하세요 :");
+        int a = s.nextInt();
+
+        if (a % 4 == 0 || a % 100 != 0 && a % 400 == 0 ) {
+            System.out.println("윤년입니다.");
+// &&는 그리고, ||는 또는, !는 ~가 아닐때.
+        }
+        else {
+            System.out.println("윤년이 아닙니다.");
+
+        }
+        s.close();
+    }
+}
