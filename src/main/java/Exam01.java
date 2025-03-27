9e7bce90a4d92d913037b05125a7e7f6b1c6f16b
kimhyengyu <sisalbkim@gmail.com>
2025-03-27 16:34:20 +0900

    시험01번

---
+++ src/main/java/Exam01.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exam01 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("첫번째 정수를 입력하세요 :");
+        int a = s.nextInt();
+        System.out.print("두번째 정수를 입력하세요 :");
+        int b = s.nextInt();
+
+        System.out.print((double) a / b) ;
+
+        s.close();
+
+    }
+}
