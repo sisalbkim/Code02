8fefdf14bcca4a99187406eeddbd729c40544ec1
kimhyengyu <sisalbkim@gmail.com>
2025-04-01 12:57:36 +0900

    -

--- src/main/java/Exam01.java
+++ src/main/java/Exam01.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
 
-public class Exam01 {
-    public static void main(String[] args) {
-
+public class Exam01{
+    public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        System.out.print("첫번째 정수를 입력하세요 :");
+
+        System.out.print("첫번째 정수를 입력하세요 : ");
         int a = s.nextInt();
-        System.out.print("두번째 정수를 입력하세요 :");
+        System.out.print("두번째 정수를 입력하세요 : ");
         int b = s.nextInt();
 
-        System.out.print((double) a / b) ;
+        System.out.println((double) a / b);
 
         s.close();
-
     }
 }
