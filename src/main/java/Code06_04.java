351097e1fd15c42b9d4f333998568470e9c9a88b
kimhyengyu <sisalbkim@gmail.com>
2025-04-04 15:37:14 +0900

    0604 반복문

---
+++ src/main/java/Code06_04.java
@@ -0,0 +1,12 @@
+public class Code06_04 {
+    public static void main(String[] args) {
+        for (int i = 10; i <=20; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 2; i <=20; i+=2){
+            System.out.print(i + " ");
+        }
+    }
+}
