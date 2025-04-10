66745ad4ced46c8d016397bc2fca40d53b0c8da3
kimhyengyu <sisalbkim@gmail.com>
2025-04-10 17:32:02 +0900

    27866

---
+++ src/main/java/Code07_06.java
@@ -0,0 +1,17 @@
+class RabbitTest {
+    String shape;
+    int xPos;
+    int yPos;
+
+    void setPosition(int x, int y) {
+        xPos = x;
+        yPos = y;
+
+    }
+}
+
+public class Code07_06 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Code07_09.java
@@ -0,0 +1,27 @@
+class Rabbit {
+    String shape;
+    int xPos;
+    int yPos;
+
+    void setPosition(int x, int y) {
+        xPos = x;
+        yPos = y;
+        System.out.println("Rabbit setPosition(" + x + ", " + y + ")");
+    }
+}
+
+public class Code07_09 {
+    public static void main(String[] args) {
+        Rabbit rabbit1 = new Rabbit();
+        Rabbit rabbit2 = new Rabbit();
+        Rabbit rabbit3 = new Rabbit();
+
+        rabbit1.shape = "원";
+        rabbit2.shape = "삼각형";
+        rabbit3.shape = "토끼";
+
+        rabbit1.setPosition(100, 100);
+        rabbit2.setPosition(-100, 100);
+        rabbit3.setPosition(0, -100);
+    }
+}
