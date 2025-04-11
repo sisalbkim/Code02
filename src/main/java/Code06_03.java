0c59b3b2a8d5944a2c61ec8289044ef0ca43ac3e
kimhyengyu <sisalbkim@gmail.com>
2025-04-11 15:53:28 +0900

    git

---
+++ src/main/java/Code06_03.java
@@ -0,0 +1,7 @@
+public class Code06_03 {
+    public static void main(String[] args) {
+        for (int a = 0; a < 3; a++) {
+            System.out.println(a + ": 난생처음 자바는 재미있습니다. ^^");
+        }
+    }
+}
