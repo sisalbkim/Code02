b94e89be5d59ee7d714e6365c16a22d9da282f58
kimhyengyu <sisalbkim@gmail.com>
2025-05-22 16:11:13 +0900

    ss

---
+++ src/main/java/Code10_03.java
@@ -0,0 +1,13 @@
+public class Code10_03 {
+    static int plus(int a, int b) {
+        int result;
+        result = a + b;
+        return result;
+    }
+    public static void main(String[] args) {
+        int hap;
+        hap = plus(100, 200);
+        System.out.println("100과 200의 plus() 메서드 결과" + hap);
+
+    }
+}
